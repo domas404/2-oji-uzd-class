bf1a633bee1de7c7a6c4f9492356a6e343c2a4d3
Windows 10 <domas.guiga@gmail.com>
2021-05-25 18:16:19 +0300

    test update

--- test/unitTest.cpp
+++ test/unitTest.cpp
@@ -1,6 +1,7 @@
 #include "../studentas.h"
-#include <gtest/gtest.h>
+#include <gtest/gtest.h>"
 
+// testuojamas klasės konstruktorius, duomenis pateikiant eiluteje
 TEST(StudentTest, nameTest){
     istringstream iss("Vardas Pavarde 1 1 5 6 4 3 1 5 7 1 7");
     Studentas stud = Studentas(iss, &Vidurkis);
@@ -8,18 +9,19 @@ TEST(StudentTest, nameTest){
     EXPECT_EQ("Pavarde", stud.getLastName());
     EXPECT_EQ(5.56F, stud.getFinal());
 }
-
+// testuojamas default klases konstruktorius ir setter'iai
 TEST(StudentTest, SetterTest){
     Studentas stud;
+    ASSERT_EQ(0, stud.getFinal()) << "Primary value is not 0";
     vector<int> grades = {10,9,7,10};
     stud.setName("Vardas");
     stud.setLastName("Pavarde");
     stud.setFinal(stud.Final(Vidurkis(4,grades),8));
-    EXPECT_EQ("Dominykas", stud.getName());
+    EXPECT_EQ("Vardas", stud.getName());
     EXPECT_EQ("Pavarde", stud.getLastName());
     EXPECT_EQ(8.4F, stud.getFinal());
 }
-
+// testuojama medianos skaiciavimo funkcija
 TEST(CalculationsTest, MedianTest){
     vector<int> grades = {8,7,6,9};
     EXPECT_EQ(7.5F, Mediana(4,grades));
